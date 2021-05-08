2e181324bc3303fa9e0c1b6531f85a2dfcd7e807
kirthika2403 <79511897+kirthika2403@users.noreply.github.com>
2021-05-08 13:36:25 +0530

    Update DonorUserCredentialsManagement.java

--- src/main/java/BloodBank/DonorUserCredentialsManagement.java
+++ src/main/java/BloodBank/DonorUserCredentialsManagement.java
@@ -3,38 +3,39 @@ package BloodBank;
 import java.util.ArrayList;
 
 public class DonorUserCredentialsManagement {
-
 	static ArrayList<DonorCredentials> donorList = new ArrayList<DonorCredentials>();
 	/**
 	 * user defined function to validate user name
 	 * @param customer
 	 * @return
 	 */    
-	public static boolean addCustomerName(DonorCredentials customer) 
-	{   boolean validity=false;
-			
-			// check customerName is not null
-			if(customer.customerName != null && customer.customerName.trim().length()>2 
-					&& customer.customerName.trim().matches("^[a-zA-Z]*$"))
-					
+	
+	public static boolean validateUserName(DonorCredentials customer)
+	{   boolean valid=false;
+		if(customer.customerName != null && customer.customerName.trim().length()>2 )
+		{
+			if(customer.customerName.trim().matches("^[a-zA-Z]*$"))
 			{
-				donorList.add(customer);
-				System.out.println(customer.customerName);
-				System.out.println("Valid credentials");
-				validity=true;
 				
+				System.out.println(customer.customerName+" Valid credentials");
+				donorList.add(customer);
+				displayCustomerList();
+				valid=true;
 				
 			}
 			else
 			{
-				System.out.println("Invalid credentials");
-				
+				System.out.println(customer.customerName+" Invalid credentials");
 			}
-			
-		    	
+		}
+		else
+		{
+			System.out.println(customer.customerName+" Invalid credentials");
+		}
+		return valid;
 		
-		return validity;
 	}
+	
 		
 	public static void displayCustomerList()
 	{   
